968985dd4f78c4b6ab7f17201db14dfce4dfdd01
DanilaKorsakov <kvashnindan@gmail.com>
2022-10-04 16:30:22 +0300

    publish text to Telegram channel

---
+++ publisher_bot.py
@@ -0,0 +1,8 @@
+import telegram
+
+
+bot = telegram.Bot(token='5747439259:AAHnp_FCXv0SodG1UboqqjuI0MgUwALpUQ8')
+
+chat_id='@image_pusblish'
+
+bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
